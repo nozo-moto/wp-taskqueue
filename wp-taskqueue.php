45b93315a6e0f45770fd20b7a2a531fdbfba4e3e
nozo-moto <nozomotoitech@gmail.com>
2019-12-14 14:40:12 +0900

    add plugin setting

---
+++ wp-taskqueue.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WP-TaskQueue
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Synchronize WooCommerce products into Next Engine
+ * Author:          nozomoto
+ * Author URI:
+ * Text Domain:     Woo Nextengine
+ * Domain Path:     /languages
+ * Version:         0.0.1
+ *
+ * @package        WP-TaskQueue
+ *
